8ae9040348548e163382c2a3df01506a71aabdea
Nworah-Gabriel <gabrielnworah6@gmail.com>
2023-04-14 00:57:08 +0100

    my task commit

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
+
+const app = express();
+
+app.get('/', ((request, response) => AppController.getHomepage(request, response)));
+app.get('/students', ((request, response) => StudentsController.getAllStudents(request, response)));
+app.get('/students/:major', ((request, response) => StudentsController.getAllStudentsByMajor(request, response)));
+
+// app.listen(1245);
+
+module.exports = app;
